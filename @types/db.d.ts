0bfe33e8a674edc88c3d74a6f63ed4f3477440ca
JM <j.maxwell.alan@gmail.com>
2023-03-11 18:41:44 -0500

    simplify db helper types

--- @types/db.d.ts
+++ @types/db.d.ts
@@ -1,14 +1,13 @@
 declare type AggregateModels = typeof import('../db/models').default;
 declare type Associator = (models: AggregateModels) => void;
-declare type Initializer = (init: ModelInitialization, models: AggregateModels) => void;
+declare type Initializer = (
+  sequelize: import('sequelize').Sequelize,
+  models: AggregateModels
+) => void;
 
 declare type UserSignupAttributes = {
   handle: string;
   displayName: string;
   email: string;
   password: string;
-};
-
-declare type ModelInitialization = {
-  sequelize: import('sequelize').Sequelize;
 };
