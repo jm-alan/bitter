e32d541959f218cab7cc1cefcad8fb1d2f49a3ea
JM <j.maxwell.alan@gmail.com>
2023-03-11 19:14:39 -0500

    comments

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -1,5 +1,8 @@
 declare global {
   namespace NodeJS {
+    // this modifies the type definition of process.env, allowing
+    // access of these properties without jumping through additional
+    // hoops
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production';
       DATABASE_URL: string;
--- db/index.ts
+++ db/index.ts
@@ -15,6 +15,10 @@ iterableModels.forEach(model => {
 });
 
 export const initialize = async () => {
+  // Tests the connection and throws if it fails
   await sequelize.authenticate();
+
+  // Forcibly updates the DB to match the shape of the models;
+  // eliminates the need for migrations in smaller projects
   await sequelize.sync({ alter: true });
 };
